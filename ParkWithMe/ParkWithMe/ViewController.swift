9e84414f193d2d0602522505d8177f7295a58ca9
Nhi Quach <nhiquach@berkeley.edu>
2016-04-16 17:09:21 -0700

    installing firebase backend

---
+++ ParkWithMe/ParkWithMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkWithMe
+//
+//  Created by Nhi Quach on 4/14/16.
+//  Copyright © 2016 Nhi Quach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
